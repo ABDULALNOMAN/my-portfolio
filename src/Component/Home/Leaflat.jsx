0a862167a4ba24b7e6e53dab3ca764de666dbe1e
Abdul Al Noman <nomanmd1st2001@gamil.com>
2023-07-16 14:01:51 +0600

    added logo & name & leaflet icon

--- src/Component/Home/Leaflat.jsx
+++ src/Component/Home/Leaflat.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker,Popup } from 'react-leaflet'
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {Icon} from 'leaflet'
 import "leaflet/dist/leaflet.css"
 const Leaflat = () => {
     return (
@@ -12,7 +14,7 @@ const Leaflat = () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[22.484626, 91.877410]}>
+            <Marker position={[22.484626, 91.877410]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
                 <Popup>
                     North Madarsha
                 </Popup>
